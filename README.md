a73f2b1d15b85b61b38c81f057165bc4351d0505
Caleb <calebharper77@gmail.com>
2024-12-08 19:49:14 -0600

    general

--- README.md
+++ README.md
@@ -4,11 +4,17 @@
 
 This is a small script/collection of scripts to be used when you need to quickly repair or reinstall something.
 
-## Utilities
+## Programs
 
 ### linux/recovery-tools.sh
 
 This runs on all Linux distros, including macOS. However, there are no tools for macOS at the moment.
+
+## Commands
+
+help - displays commands
+exit - exits the program (you can also type quit, stop, or leave it blank)
+boot-repair - installs and runs boot-repair for Linux (not supported on macOS)
 
 ## Changelog
 
--- linux/recovery-tools.sh
+++ linux/recovery-tools.sh
@@ -5,6 +5,11 @@ catch() {
     exit -1
 }
 
+quit() {
+    echo "Exiting..."
+    exit 0
+}
+
 trap catch ERR
 
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -24,40 +29,42 @@ command-input() {
     read -p ">> " user_input
     echo ""
 
-    case "$user_input" in
-        help)
-            echo "Possible commands\n"
-            echo -e "help\nShow commands\nexit\nquit\nboot-repair\nInstalls and runs boot-repair for Linux" | paste -d "\t" - - 
-            ;;
-        exit)
-            echo "Exiting..."
-            echo "Goodbye"
-            exit 0
-            ;;
-        boot-repair)
-            echo "Running boot-repair..."
-            if [ "$platform" == "darwin" ]; then
-                echo "Error: unsupported platform: $platform"
-            else
-                if ! command -v boot-repair &> /dev/null
-                then
-                    echo "Installing boot-repair..."
-                    sudo add-apt-repository ppa:yannubuntu/boot-repair
-                    sudo apt update
-                    sudo apt install -y boot-repair
-                fi
+    if [ -z "$user_input" ]; then
+        quit
+    else
+        case "$user_input" in
+            help)
+                echo "Possible commands\n"
+                echo -e "help\nShow commands\nexit\nQuits recovery-tools\nboot-repair\nInstalls and runs boot-repair for Linux" | paste -d "\t" - - 
+                ;;
+            exit|quit|stop)
+                quit
+                ;;
+            boot-repair)
+                echo "Running boot-repair..."
+                if [ "$platform" == "darwin" ]; then
+                    echo "Error: unsupported platform: $platform"
+                else
+                    if ! command -v boot-repair &> /dev/null
+                    then
+                        echo "Installing boot-repair..."
+                        sudo add-apt-repository ppa:yannubuntu/boot-repair
+                        sudo apt update
+                        sudo apt install -y boot-repair
+                    fi
 
-                echo "Starting boot-repair..."
-                boot-repair
-                echo "Exiting boot-repair..."
-            fi
-            echo "Ended boot-repair session"
-            ;;
-        *)
-            echo "Invalid command"
-            ;;
-    esac
-    command-input
+                    echo "Starting boot-repair..."
+                    boot-repair
+                    echo "Exiting boot-repair..."
+                fi
+                echo "Ended boot-repair session"
+                ;;
+            *)
+                echo "Invalid command"
+                ;;
+        esac
+        command-input
+    fi
 }
 
 command-input
